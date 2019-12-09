17fec4ae4e59a4349f8ff334c222a0753bf36486
reemrh <reemrmh97@gmail.com>
2019-12-09 15:53:28 +0200

    ui s

---
+++ app/src/main/java/com/example/noteproject/tutorial1.java
@@ -0,0 +1,14 @@
+package com.example.noteproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tutorial1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial1);
+    }
+}
